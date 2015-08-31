deaad416c73f11e7c8659e6e1b84e12b99348e0e
OneWingedShark <OneWingedShark@gmail.com>
2015-08-30 22:00:37 -0600

    Added parent for internal packages.
    Byron.Internals is the top-level for internal packages and subprograms,
    it is intended as a parent primarily to private packages.

---
+++ src/byron-internals.ads
@@ -0,0 +1,8 @@
+Pragma Ada_2012;
+Pragma Assertion_Policy( Check );
+
+-- Byron.Internals is the top-level for internal packages and subprograms, it is
+-- intended as a parent primarily to private packages.
+
+Package Byron.Internals with Pure is
+End Byron.Internals;
