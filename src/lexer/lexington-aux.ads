4458566488f416533a25ab805d769858678b1fe2
OneWingedShark <OneWingedShark@gmail.com>
2015-12-03 13:16:04 -0700

    Increased lexer subclass-enumeration from 16 to 128.
    This allows for all the keywords to be within the same class; this means
    that we have a guaranteed 256 possible 'classes' for the lexer. (Because
    we're using Natural as the ID, which gives us the following equation:
    2**15/128 = 256.)

--- src/lexer/lexington-aux.ads
+++ src/lexer/lexington-aux.ads
@@ -16,14 +16,10 @@ Package Lexington.Aux is
       --  Seperators, Group 1  --
       ---------------------------
 
-      -- Artifacts of text-files, should not be passed to the parser.
+      -- Artifacts of text-files, these should not be passed to the parser.
       Whitespace,
       Comment,
       End_of_Line,
-
-      ---------------------------
-      --  Seperators, Group 2  --
-      ---------------------------
 
       -- Single character seperators, should not be passed to the parser.
       ch_Ampersand,
@@ -44,7 +40,7 @@ Package Lexington.Aux is
       ch_Period,
 
       ---------------------------
-      --  Seperators, Group 3  --
+      --  Seperators, Group 2  --
       ---------------------------
 
       -- Meaningful seperators, may be passed to the parser.
@@ -59,10 +55,6 @@ Package Lexington.Aux is
       ss_Less_Equal,
       ss_Box,
       ss_Tick,
-
-      ---------------------------
-      --  Seperators, Group 4  --
-      ---------------------------
 
       -- Other seperators; Conceptually Non-semantic.
       ns_Open_Paren,
@@ -88,9 +80,9 @@ Package Lexington.Aux is
 
   ------  Keywords:	Reserved words as per ARM 2.9 (2).  ------------------
 
-      ------------------------
-      --  Keywords, Part 1  --
-      ------------------------
+      ----------------
+      --  Keywords  --
+      ----------------
 
       kw_Abort,
       kw_Abs,
@@ -108,11 +100,6 @@ Package Lexington.Aux is
       kw_Constant,
       kw_Declare,
       kw_Delay,
-
-      ------------------------
-      --  Keywords, Part 2  --
-      ------------------------
-
       kw_Delta,
       kw_Digits,
       kw_Do,
@@ -129,11 +116,6 @@ Package Lexington.Aux is
       kw_If,
       kw_In,
       kw_Interface,
-
-      ------------------------
-      --  Keywords, Part 3  --
-      ------------------------
-
       kw_Is,
       kw_Limited,
       kw_Loop,
@@ -150,11 +132,6 @@ Package Lexington.Aux is
       kw_Pragma,
       kw_Private,
       kw_Procedure,
-
-      ------------------------
-      --  Keywords, Part 4  --
-      ------------------------
-
       kw_Protected,
       kw_Raise,
       kw_Range,
@@ -171,11 +148,6 @@ Package Lexington.Aux is
       kw_Synchronized,
       kw_Tagged,
       kw_Task,
-
-      ------------------------
-      --  Keywords, Part 5  --
-      ------------------------
-
       kw_Terminate,
       kw_Then,
       kw_Type,
@@ -200,67 +172,63 @@ Package Lexington.Aux is
       op_Equal,
       Identifier,
 
+      Comment_Info,	-- The typical "box comment" for package-info/-headers.
+      Comment_Section,	-- A separator-/header-style comment. (A 1-line box.)
+      Comment_Block,	-- The normal "multi-line" comment.
+      Comment_Code,	-- Code disabled by comments; perhaps direct AST manip.
 
       Text,
       Nil
      )
    with Size => Natural'Size;
 
-   Class_Offset  : Constant := 16;
+   Class_Offset  : Constant := 128;
    Error_Group   : Constant := Class_Offset * 0;
-   Seperators_1  : Constant := Class_Offset * 1; -- Will not be passed to Parser.
-   Seperators_2  : Constant := Class_Offset * 2; -- Will not be passed to Parser.
-   Seperators_3  : Constant := Class_Offset * 3;
-   Seperators_4  : Constant := Class_Offset * 4;
-   Literal_Group : Constant := Class_Offset * 5;
-   Keywords_1    : Constant := Class_Offset * 6;
-   Keywords_2    : Constant := Class_Offset * 7;
-   Keywords_3    : Constant := Class_Offset * 8;
-   Keywords_4    : Constant := Class_Offset * 9;
-   Keywords_5    : Constant := Class_Offset * 10;
-   Ident_Group   : Constant := Class_Offset * 11;
+   Seperators_1  : Constant := Class_Offset * 1; -- Will not be passed to the Parser.
+   Seperators_2  : Constant := Class_Offset * 2; -- Might be passed to the Parser.
+   Literal_Group : Constant := Class_Offset * 3;
+   Keywords      : Constant := Class_Offset * 4;
+   Ident_Group   : Constant := Class_Offset * 5;
 
    for Token_ID use
      (
       Whitespace       => Seperators_1 + 0,
       Comment          => Seperators_1 + 1,
       End_of_Line      => Seperators_1 + 2,
+      ch_Ampersand     => Seperators_1 + 3,
+      ch_Astrisk       => Seperators_1 + 4,
+      ch_Apostrophy    => Seperators_1 + 5,
+      ch_Open_Paren    => Seperators_1 + 6,
+      ch_Close_Paren   => Seperators_1 + 7,
+      ch_Plus          => Seperators_1 + 8,
+      ch_Comma         => Seperators_1 + 9,
+      ch_Dash          => Seperators_1 + 10,
+      ch_Slash         => Seperators_1 + 11,
+      ch_Colon         => Seperators_1 + 12,
+      ch_Semicolon     => Seperators_1 + 13,
+      ch_Less_Than     => Seperators_1 + 14,
+      ch_Greater_Than  => Seperators_1 + 15,
+      ch_Equal         => Seperators_1 + 16,
+      ch_Quote         => Seperators_1 + 17,
+      ch_Period        => Seperators_1 + 18,
 
-      ch_Ampersand     => Seperators_2 + 0,
-      ch_Astrisk       => Seperators_2 + 1,
-      ch_Apostrophy    => Seperators_2 + 2,
-      ch_Open_Paren    => Seperators_2 + 3,
-      ch_Close_Paren   => Seperators_2 + 4,
-      ch_Plus          => Seperators_2 + 5,
-      ch_Comma         => Seperators_2 + 6,
-      ch_Dash          => Seperators_2 + 7,
-      ch_Slash         => Seperators_2 + 8,
-      ch_Colon         => Seperators_2 + 9,
-      ch_Semicolon     => Seperators_2 + 10,
-      ch_Less_Than     => Seperators_2 + 11,
-      ch_Greater_Than  => Seperators_2 + 12,
-      ch_Equal         => Seperators_2 + 13,
-      ch_Quote         => Seperators_2 + 14,
-      ch_Period        => Seperators_2 + 15,
-
-      ss_Assign        => Seperators_3 + 0,
-      ss_Arrow         => Seperators_3 + 1,
-      ss_Open_Label    => Seperators_3 + 2,
-      ss_Close_Label   => Seperators_3 + 3,
-      ss_Dillipsis     => Seperators_3 + 4,
-      ss_Exponent      => Seperators_3 + 5,
-      ss_Not_Equal     => Seperators_3 + 6,
-      ss_Greater_Equal => Seperators_3 + 7,
-      ss_Less_Equal    => Seperators_3 + 8,
-      ss_Box           => Seperators_3 + 9,
-      ss_Tick          => Seperators_3 + 10,
-
-      ns_Open_Paren    => Seperators_4 + 0,
-      ns_Close_Paren   => Seperators_4 + 1,
-      ns_Comma         => Seperators_4 + 2,
-      ns_Colon         => Seperators_4 + 3,
-      ns_Semicolon     => Seperators_4 + 4,
-      ns_Period        => Seperators_4 + 5,
+      ss_Assign        => Seperators_2 + 0,
+      ss_Arrow         => Seperators_2 + 1,
+      ss_Open_Label    => Seperators_2 + 2,
+      ss_Close_Label   => Seperators_2 + 3,
+      ss_Dillipsis     => Seperators_2 + 4,
+      ss_Exponent      => Seperators_2 + 5,
+      ss_Not_Equal     => Seperators_2 + 6,
+      ss_Greater_Equal => Seperators_2 + 7,
+      ss_Less_Equal    => Seperators_2 + 8,
+      ss_Box           => Seperators_2 + 9,
+      ss_Tick          => Seperators_2 + 10,
+      ns_Open_Paren    => Seperators_2 + 11,
+      ns_Close_Paren   => Seperators_2 + 12,
+      ns_Comma         => Seperators_2 + 13,
+      ns_Colon         => Seperators_2 + 14,
+      ns_Semicolon     => Seperators_2 + 15,
+      ns_Period        => Seperators_2 + 16,
 
       li_String        => Literal_Group + 0,
       li_Rational      => Literal_Group + 1,
@@ -270,83 +238,79 @@ Package Lexington.Aux is
       li_Based_Integer => Literal_Group + 5,
       li_Based_Float   => Literal_Group + 6,
 
-      kw_Abort         => Keywords_1 + 0,
-      kw_Abs           => Keywords_1 + 1,
-      kw_Abstract      => Keywords_1 + 2,
-      kw_Accept        => Keywords_1 + 3,
-      kw_Access        => Keywords_1 + 4,
-      kw_Aliased       => Keywords_1 + 5,
-      kw_All           => Keywords_1 + 6,
-      kw_And           => Keywords_1 + 7,
-      kw_Array         => Keywords_1 + 8,
-      kw_At            => Keywords_1 + 9,
-      kw_Begin         => Keywords_1 + 10,
-      kw_Body          => Keywords_1 + 11,
-      kw_Case          => Keywords_1 + 12,
-      kw_Constant      => Keywords_1 + 13,
-      kw_Declare       => Keywords_1 + 14,
-      kw_Delay         => Keywords_1 + 15,
-
-      kw_Delta         => Keywords_2 + 0,
-      kw_Digits        => Keywords_2 + 1,
-      kw_Do            => Keywords_2 + 2,
-      kw_Else          => Keywords_2 + 3,
-      kw_Elsif         => Keywords_2 + 4,
-      kw_End           => Keywords_2 + 5,
-      kw_Entry         => Keywords_2 + 6,
-      kw_Exception     => Keywords_2 + 7,
-      kw_Exit          => Keywords_2 + 8,
-      kw_For           => Keywords_2 + 9,
-      kw_Function      => Keywords_2 + 10,
-      kw_Generic       => Keywords_2 + 11,
-      kw_Goto          => Keywords_2 + 12,
-      kw_If            => Keywords_2 + 13,
-      kw_In            => Keywords_2 + 14,
-      kw_Interface     => Keywords_2 + 15,
-
-      kw_Is            => Keywords_3 + 0,
-      kw_Limited       => Keywords_3 + 1,
-      kw_Loop          => Keywords_3 + 2,
-      kw_Mod           => Keywords_3 + 3,
-      kw_New           => Keywords_3 + 4,
-      kw_Not           => Keywords_3 + 5,
-      kw_Null          => Keywords_3 + 6,
-      kw_Of            => Keywords_3 + 7,
-      kw_Or            => Keywords_3 + 8,
-      kw_Others        => Keywords_3 + 9,
-      kw_Out           => Keywords_3 + 10,
-      kw_Overriding    => Keywords_3 + 11,
-      kw_Package       => Keywords_3 + 12,
-      kw_Pragma        => Keywords_3 + 13,
-      kw_Private       => Keywords_3 + 14,
-      kw_Procedure     => Keywords_3 + 15,
-
-      kw_Protected     => Keywords_4 + 0,
-      kw_Raise         => Keywords_4 + 1,
-      kw_Range         => Keywords_4 + 2,
-      kw_Record        => Keywords_4 + 3,
-      kw_Rem           => Keywords_4 + 4,
-      kw_Renames       => Keywords_4 + 5,
-      kw_Requeue       => Keywords_4 + 6,
-      kw_Return        => Keywords_4 + 7,
-      kw_Reverse       => Keywords_4 + 8,
-      kw_Select        => Keywords_4 + 9,
-      kw_Separate      => Keywords_4 + 10,
-      kw_Some          => Keywords_4 + 11,
-      kw_Subtype       => Keywords_4 + 12,
-      kw_Synchronized  => Keywords_4 + 13,
-      kw_Tagged        => Keywords_4 + 14,
-      kw_Task          => Keywords_4 + 15,
-
-      kw_Terminate     => Keywords_5 + 0,
-      kw_Then          => Keywords_5 + 1,
-      kw_Type          => Keywords_5 + 2,
-      kw_Until         => Keywords_5 + 3,
-      kw_Use           => Keywords_5 + 4,
-      kw_When          => Keywords_5 + 5,
-      kw_While         => Keywords_5 + 6,
-      kw_With          => Keywords_5 + 7,
-      kw_Xor           => Keywords_5 + 8,
+      kw_Abort         => Keywords + 0,
+      kw_Abs           => Keywords + 1,
+      kw_Abstract      => Keywords + 2,
+      kw_Accept        => Keywords + 3,
+      kw_Access        => Keywords + 4,
+      kw_Aliased       => Keywords + 5,
+      kw_All           => Keywords + 6,
+      kw_And           => Keywords + 7,
+      kw_Array         => Keywords + 8,
+      kw_At            => Keywords + 9,
+      kw_Begin         => Keywords + 10,
+      kw_Body          => Keywords + 11,
+      kw_Case          => Keywords + 12,
+      kw_Constant      => Keywords + 13,
+      kw_Declare       => Keywords + 14,
+      kw_Delay         => Keywords + 15,
+      kw_Delta         => Keywords + 16,
+      kw_Digits        => Keywords + 17,
+      kw_Do            => Keywords + 18,
+      kw_Else          => Keywords + 19,
+      kw_Elsif         => Keywords + 20,
+      kw_End           => Keywords + 21,
+      kw_Entry         => Keywords + 22,
+      kw_Exception     => Keywords + 23,
+      kw_Exit          => Keywords + 24,
+      kw_For           => Keywords + 25,
+      kw_Function      => Keywords + 26,
+      kw_Generic       => Keywords + 27,
+      kw_Goto          => Keywords + 28,
+      kw_If            => Keywords + 29,
+      kw_In            => Keywords + 30,
+      kw_Interface     => Keywords + 31,
+      kw_Is            => Keywords + 32,
+      kw_Limited       => Keywords + 33,
+      kw_Loop          => Keywords + 34,
+      kw_Mod           => Keywords + 35,
+      kw_New           => Keywords + 36,
+      kw_Not           => Keywords + 37,
+      kw_Null          => Keywords + 38,
+      kw_Of            => Keywords + 39,
+      kw_Or            => Keywords + 40,
+      kw_Others        => Keywords + 41,
+      kw_Out           => Keywords + 42,
+      kw_Overriding    => Keywords + 43,
+      kw_Package       => Keywords + 44,
+      kw_Pragma        => Keywords + 45,
+      kw_Private       => Keywords + 46,
+      kw_Procedure     => Keywords + 47,
+      kw_Protected     => Keywords + 48,
+      kw_Raise         => Keywords + 49,
+      kw_Range         => Keywords + 50,
+      kw_Record        => Keywords + 51,
+      kw_Rem           => Keywords + 52,
+      kw_Renames       => Keywords + 53,
+      kw_Requeue       => Keywords + 54,
+      kw_Return        => Keywords + 55,
+      kw_Reverse       => Keywords + 56,
+      kw_Select        => Keywords + 57,
+      kw_Separate      => Keywords + 58,
+      kw_Some          => Keywords + 59,
+      kw_Subtype       => Keywords + 60,
+      kw_Synchronized  => Keywords + 61,
+      kw_Tagged        => Keywords + 62,
+      kw_Task          => Keywords + 63,
+      kw_Terminate     => Keywords + 64,
+      kw_Then          => Keywords + 65,
+      kw_Type          => Keywords + 66,
+      kw_Until         => Keywords + 67,
+      kw_Use           => Keywords + 68,
+      kw_When          => Keywords + 69,
+      kw_While         => Keywords + 70,
+      kw_With          => Keywords + 71,
+      kw_Xor           => Keywords + 72,
 
       op_Mul           => Ident_Group + 0,
       op_Div           => Ident_Group + 1,
@@ -358,12 +322,20 @@ Package Lexington.Aux is
       op_Equal         => Ident_Group + 7,
       Identifier       => Ident_Group + 8,
 
+      Comment_Info     => ID_For_Text_Token - 4,
+      Comment_Section  => ID_For_Text_Token - 3,
+      Comment_Block    => ID_For_Text_Token - 2,
+      Comment_Code     => ID_For_Text_Token - 1,
       Text             => ID_For_Text_Token,
       Nil              => ID_For_Null_Token
      );
 
+   Subtype Noncomment is Token_ID range ss_Assign..Identifier;
+
+   Subtype Emitable_Comments is Token_ID range Comment_Info..Comment_Code;
+
    Subtype Emitable is Token_ID
-   with Static_Predicate => Emitable in ss_Assign..Identifier;
+   with Static_Predicate => Emitable in Noncomment | Emitable_Comments;
 
 
    Package Token_Pkg is new Lexington.Parameters( Token_ID );
