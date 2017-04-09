6672e6003243312a2ca71740fba437f6ed6ad3e8
OneWingedShark <OneWingedShark@gmail.com>
2017-04-08 20:46:17 -0600

    Minor updates, annotation.

--- src/internals/spark/byron-internals-spark-element.adb
+++ src/internals/spark/byron-internals-spark-element.adb
@@ -3,16 +3,16 @@ Pragma Assertion_Policy( Check );
 
 Package Body Byron.Internals.SPARK.Element with SPARK_Mode => On is
 
-    Function Transformation_Function( Input : Element_Type ) Return Element_Type is
+    Function Transformation( Input : Element_Type ) Return Element_Type is
 	-- The input is not mutable here, so we need to use an Identity to
 	-- get a mutable copy.
 	Object : Element_Type := Copy(Input);
     begin
 	-- We then apply the given function.
 	Transform( Object );
-	-- And return the identity of the result. (Becase we have no assignment.)
-	Return Result : constant Element_Type:= Copy( Object );
-    end Transformation_Function;
+	-- And return a copy of the result. (Becase we have no assignment.)
+	Return Result : Constant Element_Type:= Copy( Object );
+    End Transformation;
 
 
     Procedure Transform( Input : in out Element_Type ) is
--- src/internals/spark/byron-internals-spark-element.ads
+++ src/internals/spark/byron-internals-spark-element.ads
@@ -3,22 +3,24 @@ Pragma Assertion_Policy( Check );
 
 Generic
     Type Element_Type(<>) is limited private;
-    with Function Copy( Input : Element_Type ) return Element_Type;
-Package Byron.Internals.SPARK.Element with SPARK_Mode => On is
-
+    with Function Copy( Input : Element_Type ) return Element_Type is <> with Pure_Function;
+Package Byron.Internals.SPARK.Element with Pure, SPARK_Mode => On is
 
     -- Takes T1 and T2; applies them in order: T1, T2.
     Generic
 	with Procedure T1( Input : in out Element_Type ) is null;
 	with Procedure T2( Input : in out Element_Type ) is null;
-    Procedure Transform  ( Input : in out Element_Type ) with Inline;
+    Procedure Transform  ( Input : in out Element_Type )
+    with Inline, Global => Null, Depends => (Input => Input);
+
 
 
     -- Takse a transformation-procedure and returns am applying-function.
     Generic
 	with Procedure Transform(Input : in out Element_Type) is null;
-    Function Transformation_Function( Input : Element_Type ) Return Element_Type
-    with Pure_Function, Inline;
+    Function Transformation( Input : Element_Type ) Return Element_Type
+    with Pure_Function, Inline, Global => Null,
+         Depends => (Transformation'Result => Input);
 
 
     -- Takes F & G returning G( F() ).
@@ -26,7 +28,8 @@ Package Byron.Internals.SPARK.Element with SPARK_Mode => On is
 	with function F( Input : Element_Type ) Return Element_Type;
 	with function G( Input : Element_Type ) Return Element_Type;
     Function Compose( Input : Element_Type ) Return Element_Type
-    with Pure_Function, Inline;
+    with Pure_Function, Inline, Global => Null,
+         Depends => (Compose'Result => Input);
 
 
 End Byron.Internals.SPARK.Element;
